a1d8849382db524405a980d2e4aac160bd5dd0a8
Anish Joshi <anishjoshi2056@hotmail.com>
2020-09-30 13:45:52 +0545

    complete todo React App by watching webdevsimplified

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-function Todo() {
+function Todo({todo,toggleTodo}) {
+
+    function handleTodoClick() {
+        toggleTodo(todo.id)
+      }
+      
     return (
         <div>
             <label>
-                <input type="checkbox"/>
-                Hi there!!
+                <input type="checkbox" checked={todo.complete} onChange={handleTodoClick}/>
+                {todo.name}
             </label>
         </div>
     )
