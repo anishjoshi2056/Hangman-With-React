091e6938a8c9ad3af405c0ade8b81800ca0a380f
Anish Joshi <anishjoshi2056@hotmail.com>
2020-10-01 12:21:52 +0545

    first phase done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-//Learn Use Effect
-import React, {useState,useEffect} from 'react';
-
-function App() {
-  const [windowWidth,setWindowWidth] = useState(window.innerWidth);
-
-  const handleResize = () =>{
-    setWindowWidth(window.innerWidth);
-  }
-  useEffect(()=>{
-    window.addEventListener('resize',handleResize)
-    return () => {
-      window.removeEventListener('resize',handleResize);
-    }
-  },[])
-  return (
-   <div>
-     {windowWidth}
-   </div>
-  );
+import React from 'react'
+import Header from './components/Header'
+import Figure from './components/Figure'
+import WrongLetters from './components/WrongLetters'
+import Word from './components/Word'
+import Popup from './components/Popup'
+import Notification from './components/Notification'
+import './App.css';
+function App(){
+  return(
+    <>
+    <Header/>
+    <Figure className='game-container'/>
+    <WrongLetters/>
+    <Word/>
+    <Popup/>
+    <Notification/>
+    </>
+  )
 }
-
 export default App;
---
+++ src/components/WrongLetters.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WrongLetters() {
+    return (
+        <div class="word" id="word"></div>
+    )
+}
+
+export default WrongLetters
