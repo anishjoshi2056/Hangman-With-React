7cc5bda9a6933757b542e1603b54f94b212491e5
Anish Joshi <anishjoshi2056@hotmail.com>
2020-10-01 08:30:20 +0545

    another example of useEffect hook

--- src/App.js
+++ src/App.js
@@ -2,23 +2,20 @@
 import React, {useState,useEffect} from 'react';
 
 function App() {
- const [resourceType,setResourceType] = useState('posts')
- const [items,setItems] = useState([]);
+  const [windowWidth,setWindowWidth] = useState(window.innerWidth);
 
+  const handleResize = () =>{
+    setWindowWidth(window.innerWidth);
+  }
   useEffect(()=>{
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-    .then(response => response.json())
-    .then(json => setItems(json))
-  },[resourceType])
+    window.addEventListener('resize',handleResize)
+    return () => {
+      window.removeEventListener('resize',handleResize);
+    }
+  },[])
   return (
    <div>
-     <button onClick={()=> setResourceType('posts')}>Posts</button>
-     <button onClick={()=> setResourceType('users')}>Users</button>
-     <button onClick={()=> setResourceType('comments')}>Comments</button>
-     <h1>{resourceType}</h1>
-     {items.map(item => {
-       return <pre>{JSON.stringify(items)}</pre>
-     })}
+     {windowWidth}
    </div>
   );
 }
