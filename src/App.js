ad06109a04cf87fdb27a1af03f650caeede364eb
Anish Joshi <anishjoshi2056@hotmail.com>
2020-10-01 08:20:39 +0545

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,52 +1,25 @@
-import React, {useState,useRef , useEffect} from 'react';
-import TodoList from './TodoList';
-import { v4 as uuidv4 } from 'uuid';
-
-const LOCAL_STORAGE_KEY = 'todoApp.todos'
+//Learn Use Effect
+import React, {useState,useEffect} from 'react';
 
 function App() {
-  const [todos,setTodos] = useState([])
-  const todoNameRef = useRef()
+ const [resourceType,setResourceType] = useState('posts')
+ const [items,setItems] = useState([]);
 
-  useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
-    if (storedTodos) setTodos(storedTodos)
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos])
-
-//Toggle the checkbox
-  function toggleTodo(id){
-    const newTodos = [...todos]
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
-  }
-//to add new todos
-  function handleAddTodo(e){
-    const name = todoNameRef.current.value;
-    if(name === '') return
-    setTodos(prevTodos => {
-      return [...prevTodos, {id: uuidv4(),name:name,complete:false}]
-    })
-    todoNameRef.current.value = null
-  }
-//clear all todos
-function handleClearTodos() {
-  const newTodos = todos.filter(todo => !todo.complete)
-  setTodos(newTodos)
-}
-
-  
+  useEffect(()=>{
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+    .then(response => response.json())
+    .then(json => setItems(json))
+  },[resourceType])
   return (
-    <>
-     <TodoList todos={todos} toggleTodo={toggleTodo}/>
-      <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>Add Todo</button>
-      <button onClick={handleClearTodos}>Clear Complete</button>
-    </>
+   <div>
+     <button onClick={()=> setResourceType('posts')}>Posts</button>
+     <button onClick={()=> setResourceType('users')}>Users</button>
+     <button onClick={()=> setResourceType('comments')}>Comments</button>
+     <h1>{resourceType}</h1>
+     {items.map(item => {
+       return <pre>{JSON.stringify(items)}</pre>
+     })}
+   </div>
   );
 }
 
